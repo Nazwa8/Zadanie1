69326c74813d79c272c8ff0052a1b35728f81a66
Nazwa8 <matiskowron2006@gmail.com>
2024-10-10 14:12:23 +0200

    1 zadanie skończone

--- app/src/main/java/com/example/zadanie1/MainActivity.java
+++ app/src/main/java/com/example/zadanie1/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.zadanie1;
 
+import android.widget.TextView;
+import android.widget.Toast;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -13,22 +15,48 @@ import androidx.core.view.WindowInsetsCompat;
 public class MainActivity extends AppCompatActivity {
 
     private Button klik; //deklaracja przycisku
+    private Button klik2;
+
+    private int licznik;
+
+    private TextView counter;
 
     @Override
     protected void onCreate(Bundle saveInstanceState) {
         super.onCreate(saveInstanceState);
         setContentView(R.layout.activity_main);
 
+
         //Inicjalizacja przycisku poprzez od z pliku XML
-        klik = findViewById(R.id.klik);
+        klik = findViewById(R.id.p1);
+
+        klik2 = findViewById(R.id.p2);
+
+        counter = findViewById(R.id.counter);
 
         //Ustawienie nasłuchiwania na kliknięcie przycisku
         klik.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 klik.setEnabled(false);
+                licznik++;
+                counter.setText("Counter: " + licznik);
+                Toast.makeText(MainActivity.this, "Przycisk został kliknięty", Toast.LENGTH_SHORT).show();
+            }
+
+        });
+
+        klik2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                klik.setEnabled(true);
+                Toast.makeText(MainActivity.this, "Przycisk został kliknięty", Toast.LENGTH_SHORT).show();
 
             }
         });
+
+
+
     }
+
 }
